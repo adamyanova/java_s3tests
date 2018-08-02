21bb0d00447309c2b1a87f7ca09e6f96b18a81f2
Antoaneta Damyanova <adamyanova@gmail.com>
2018-08-02 14:09:54 +0200

    teardown retries in befoe method
    Signed-off-by: Antoaneta Damyanova <adamyanova@gmail.com>

--- src/test/java/AWS4Test.java
+++ src/test/java/AWS4Test.java
@@ -67,6 +67,7 @@ public class AWS4Test {
 	@BeforeMethod
 	public void setUp() throws Exception {
 		System.out.printf("Cleanup Before %n");
+		utils.teradownRetries = 0;
 		utils.tearDown(svc);
 	}
 
